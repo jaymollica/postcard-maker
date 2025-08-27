3ba928adfac4590b7b5ff7190f59c104a3ae083d
Jay Mollica <jaymollica@gmail.com>
2025-08-27 13:07:12 -0400

    action update

--- embed/lobby.js
+++ embed/lobby.js
@@ -1,1 +1,105 @@
-const BACKEND_URL="http://localhost:9999",FRONTEND_ORIGIN="localhost:3000";function Lobby({button:n,canvas:t,...i}={}){if(this._send=async function({canvas:t,nonce:n,optionalParams:e}){t=(t=t instanceof HTMLElement==!1&&"string"==typeof t?document.querySelector(t):t).toDataURL("image/jpeg"),t=(await(await fetch(BACKEND_URL+"/img",{method:"POST",body:JSON.stringify({items:[{id:"postcard-4x6"}],imageData:t,nonce:n}),credentials:"same-origin",cache:"no-cache",redirect:"follow",headers:{"Content-Type":"application/json","Access-Control-Allow-Origin":window.location.host}})).json()).url;if(t){let n="//"+FRONTEND_ORIGIN+"?imgUrl="+encodeURIComponent(t);void 0!==e&&0<Object.keys(e).length&&(n+="&optionalParams="+btoa(JSON.stringify(e))),n+="&artistUrl="+encodeURIComponent(window.location.origin),window.open(n,"_self")}},this._buttonClickHandler=async function(n,t){try{t.preventDefault(),this.button.classList.add("disable");var{canvas:e,nonce:o}=n;await this._send({canvas:e,nonce:o,optionalParams:i})}catch(n){console.error("An error occurred:",n)}},this._getNonce=async function(n=!1){try{var t=await(await fetch(BACKEND_URL+"/nonce",{method:"POST",credentials:"same-origin",cache:"no-cache",redirect:"follow",headers:{"Content-Type":"application/json","Access-Control-Allow-Origin":window.location.host}})).json();if(this.nonce=t.nonce,n)return t.nonce}catch(n){console.error(n)}},n instanceof HTMLElement==!1&&"string"==typeof n&&0<n.length)this.button=document.querySelector(n);else{if(void 0===n||0===n.length)return{send:this._send,getNonce:this._getNonce};this.button=thing}this.canvas=t instanceof HTMLElement==!1&&"string"==typeof t?document.querySelector(t):t,this.button.addEventListener("click",this._buttonClickHandler.bind(null,this),{once:!0}),document.addEventListener("DOMContentLoaded",this._getNonce.bind(null,this))}
+const BACKEND_URL = 'http://localhost:9999';
+const FRONTEND_ORIGIN = 'localhost:3000';
+
+function Lobby({button, canvas,  ...optionalParams} = {}){
+
+	this._send = async function({canvas, nonce, optionalParams}){
+		canvas = canvas instanceof HTMLElement === false && typeof canvas === 'string' ? document.querySelector(canvas) : canvas;
+
+		const imageData = canvas.toDataURL('image/jpeg');
+	
+		const res = await fetch(BACKEND_URL + '/img', {
+			method: "POST",
+			body: JSON.stringify({
+				items: [
+					{
+						id: "postcard-4x6"
+					}
+				],
+				imageData: imageData,
+				nonce: nonce
+			}),
+			credentials: 'same-origin',
+			cache: 'no-cache',
+			redirect: 'follow',
+			headers: {
+				"Content-Type": "application/json",
+				'Access-Control-Allow-Origin': window.location.host
+			}
+		});
+
+		const data = await res.json();
+
+		// this will be replaced by AWS call to store img object & get URL
+		const imageUrl = data.url;
+		if( imageUrl ){
+			let url = "//" + FRONTEND_ORIGIN + "?imgUrl=" + encodeURIComponent(imageUrl);
+			if( typeof optionalParams !== 'undefined' && Object.keys(optionalParams).length > 0 ){
+				url += '&optionalParams=' + btoa(JSON.stringify(optionalParams));
+			}
+			url += '&artistUrl=' + encodeURIComponent(window.location.origin);
+			window.open(url, "_self");
+		}
+	}
+
+	this._buttonClickHandler = async function(context, e) {
+		try {
+			e.preventDefault();
+			this.button.classList.add('disable');
+			const { canvas, nonce } = context;
+	
+			await this._send({
+				canvas : canvas,
+				nonce: nonce,
+				optionalParams, optionalParams
+			});
+		} catch (error) {
+			console.error('An error occurred:', error);
+		}
+	}
+
+	this._getNonce = async function(returnNonce = false){
+		try {
+			const res = await fetch(BACKEND_URL + '/nonce', {
+				method: "POST",
+				credentials: 'same-origin',
+				cache: 'no-cache',
+				redirect: 'follow',
+				headers: {
+					"Content-Type": "application/json",
+					'Access-Control-Allow-Origin': window.location.host
+				}
+			});
+
+			const data = await res.json();
+
+			this.nonce = data.nonce;
+
+			if( returnNonce ){
+				return data.nonce;
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	// make the button & canvas objects
+	if (button instanceof HTMLElement === false && typeof button === 'string' && button.length > 0) {
+		this.button = document.querySelector(button);
+	}
+	else if ( typeof button === 'undefined' || button.length === 0 ){
+		return {
+			send : this._send,
+			getNonce: this._getNonce,
+		}
+	}
+	else {
+		this.button = thing;
+	}
+
+	this.canvas = canvas instanceof HTMLElement === false && typeof canvas === 'string' ? document.querySelector(canvas) : canvas;
+
+	this.button.addEventListener('click', this._buttonClickHandler.bind(null, this), {once : true});
+
+	document.addEventListener('DOMContentLoaded', this._getNonce.bind(null, this));
+}
